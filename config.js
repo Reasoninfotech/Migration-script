96dee01611103e824aac965af58c07b5503f90e1
reason <gunjan.reason@gmail.com>
2026-05-11 14:58:43 +0530

    complete migration

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 export const CONFIG = {
   // Source Store Configuration
   source: {
-    shop: "tg-work.myshopify.com",
-    accessToken: "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",
+    shop: "",
+    accessToken: "",
     apiVersion: "2024-04"
   },
 
   // Target Store Configuration
   target: {
-    shop: "gk-work.myshopify.com",
-    accessToken: "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",
+    shop: "",
+    accessToken: "",
     apiVersion: "2024-04"
   }
 };
